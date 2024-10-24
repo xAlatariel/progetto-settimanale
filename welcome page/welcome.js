8cd4c3a8dcf2319d29b310e265d0c88901694e62
xAlatariel <Orlandielia82@gmail.com>
2024-10-24 05:33:02 +0200

    connessione-welcome-to-benchmark

--- welcome page/welcome.js
+++ welcome page/welcome.js
@@ -3,7 +3,7 @@ const button = document.getElementById ("acceptButton");
 
 button.addEventListener ("click", function (){
     if (checkbox.checked == true){
-        window.location.href = "https://learn.epicode.com/"
+        window.location.href = "/benchmark page/benchmark.html"
     } else { 
         alert("Accept the conditions")
     }
